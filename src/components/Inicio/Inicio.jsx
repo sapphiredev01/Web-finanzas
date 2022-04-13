2bc7deb4111894687ceac3772d861a648d8141e3
Carlos Vargas <varlopecar@gmail.com>
2022-04-13 15:43:33 -0500

    Trabjando en inicio

--- src/components/Inicio/Inicio.jsx
+++ src/components/Inicio/Inicio.jsx
@@ -1,5 +1,8 @@
 import styled from "styled-components";
 import Wrapper from "../UI/Wrapper";
+import { Container, MediaItem } from "../UI/Media";
+import { Hexagon, Image } from "./Hexagon";
+import { FaFacebookF, AiOutlineInstagram, BsWhatsapp, HiOutlineMail } from "react-icons/all";
 
 const Title = styled.h1`
   text-align: left;
@@ -18,11 +21,35 @@ const Content = styled.div`
   opacity: 1;
 `;
 
+
+
 const Inicio = () => {
   return (
     <Wrapper>
+      <Container>
+        <MediaItem>
+          <FaFacebookF />
+        </MediaItem>
+        <MediaItem>
+          <AiOutlineInstagram />
+        </MediaItem>
+        <MediaItem>
+          <BsWhatsapp />
+        </MediaItem>
+        <MediaItem>
+          <HiOutlineMail />
+        </MediaItem>
+      </Container>
+      <Hexagon>
+        <Image src="src/images/business.jpg" />
+      </Hexagon>
       <Title>El crecimiento esta asegurado.</Title>
-      <Content>Especialistas en financiamiento a la medida para su negocio , contamos con experiencia apoyando a las empresas a obtener créditos a través de instituciones internacionales.</Content>
+      <Content>
+        Especialistas en financiamiento a la medida para su negocio , contamos
+        con experiencia apoyando a las empresas a obtener créditos a través de
+        instituciones internacionales.
+      </Content>
+
     </Wrapper>
   );
 };
---
+++ src/components/UI/Media.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+    display: flex;
+    text-align: right;
+    font-size: 1.5rem;
+    flex-direction: row;
+    justify-content: flex-end;
+    width: 100%;
+`;
+
+const MediaItem = styled.div`
+    margin: 0 2rem;
+    font-size: 2rem;
+`;
+
+export { Container, MediaItem };
