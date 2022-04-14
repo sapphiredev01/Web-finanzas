a019e174353d41655ccc633983e827426a759248
Chris-MuGa <Chrisalmuga@Hotmail.com>
2022-04-13 19:03:25 -0500

    Avance1

--- src/components/Nosotros/Nosotros.jsx
+++ src/components/Nosotros/Nosotros.jsx
@@ -3,18 +3,36 @@ import Wrapper from "../UI/Wrapper";
 
 const Title = styled.h1`
   text-align: left;
-  font: 82px/100px Helvetica Neue;
+  font: bold 50px Helvetica;
   letter-spacing: 0px;
   color: #000000;
   opacity: 1;
+  position: relative;
+  top: 90px;
+  left: 130px;
+  width: 400px;
+  height: 150px;
 `;
 
 const Content = styled.div`
   text-align: left;
-  font: Helvetica Neue;
+  font: bold 16px Helvetica;
   letter-spacing: 0px;
   color: #000000;
+  text-transform: lowercase;
   opacity: 1;
+  position relative;
+  top: 100px;
+  left: 130px;
+  width: 525px;
+  height: 150px;
+`;
+
+const imgHor = styled.img`
+width="300px";
+height="200px";
+border: 1px solid blue;
+position: absolute;
 `;
 
 const Nosotros = () => {
@@ -22,10 +40,11 @@ const Nosotros = () => {
     <Wrapper>
       <Title>Los expertos que necesitas.</Title>
       <Content>
-        Especialistas en financiamiento a la medida para su negocio , contamos
-        con experiencia apoyando a las empresas a obtener créditos a través de
-        instituciones internacionales.
+        Somos un equipo de operadores experimentados, comprometidos con buscar y
+        detectar las mejores Oportunidades de inversión de mercado, condiciones
+        financieras de las acciones y los mercados estadounidenses de divisas.
       </Content>
+      <imgHor/>
     </Wrapper>
   );
 };
