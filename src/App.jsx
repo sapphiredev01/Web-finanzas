f8596630eedd16037cc40369ff05033ef7954632
Chris-MuGa <Chrisalmuga@Hotmail.com>
2022-04-26 21:01:39 -0500

    Creación de modulo

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,13 @@ import React from 'react';
 import Inicio from "./components/Inicio/Inicio"
 import Nosotros from "./components/Nosotros/Nosotros"
 import Servicios from "./components/Servicios/Servicios"
+import Contacto from "./components/Contacto/Contacto"
 
 
 function App() {
   return (
     <div>
-      <Inicio />
+      <Contacto />
     </div>
   )
 }
