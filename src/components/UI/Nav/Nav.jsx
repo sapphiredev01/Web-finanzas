76512a320cd4d0296c5019e475734fa38d065f6e
Chris-MuGa <Chrisalmuga@Hotmail.com>
2022-05-02 12:55:03 -0500

    Pequeña correccióon visual

--- src/components/UI/Nav/Nav.jsx
+++ src/components/UI/Nav/Nav.jsx
@@ -6,34 +6,22 @@ const Nav = () => {
     <Wrapper>
       <Stepper>
         <Step>
-          <a href="#inicio">
             <Title>INICIO</Title>
-          </a>
         </Step>
         <Step>
-          <a href="#nosotros">
             <Title>NOSOTROS</Title>
-          </a>
         </Step>
         <Step>
-          <a href="#inicio">
             <Title>SERVICIOS</Title>
-          </a>
         </Step>
         <Step>
-          <a href="#paquetes">
             <Title>PAQUETES</Title>
-          </a>
         </Step>
         <Step>
-          <a href="#calculadora">
             <Title>CALCULADORA</Title>
-          </a>
         </Step>
         <Step>
-          <a href="#contacto">
             <Title>CONTACTO</Title>
-          </a>
         </Step>
       </Stepper>
     </Wrapper>
