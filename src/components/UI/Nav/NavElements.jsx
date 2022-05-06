d8801828a9c7ff1a0884ed61ff43ad506bb44bb5
Naruxtian <Naruxdev@Gmail.com>
2022-05-06 16:50:39 -0500

    Ocultamiento de navbar responsive

--- src/components/UI/Nav/NavElements.jsx
+++ src/components/UI/Nav/NavElements.jsx
@@ -1,11 +1,36 @@
 import styled from "styled-components";
 import { Link } from "react-scroll";
 
+const size = {
+  mobileS: "320px",
+  mobileM: "375px",
+  mobileL: "425px",
+  tablet: "768px",
+  laptop: "1024px",
+  laptopL: "1440px",
+  desktop: "2560px",
+};
+
+const device = {
+  mobileS: `(max-width: ${size.mobileS})`,
+  mobileM: `(max-width: ${size.mobileM})`,
+  mobileL: `(max-width: ${size.mobileL})`,
+  tablet: `(max-width: ${size.tablet})`,
+  laptop: `(max-width: ${size.laptop})`,
+  laptopL: `(max-width: ${size.laptopL})`,
+  desktop: `(max-width: ${size.desktop})`,
+  desktopL: `(max-width: ${size.desktop})`,
+};
+
 export const Wrapper = styled.div`
   position: fixed;
   height: 100%;
   display: flex;
   align-items: center;
+
+  @media ${device.tablet} {
+    display: none;
+  }
 `;
 
 export const Stepper = styled.ol`
