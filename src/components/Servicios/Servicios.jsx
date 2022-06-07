f22d02db580931e4989317d4faec1f3278715a26
Naruxtian <Naruxdev@Gmail.com>
2022-06-07 15:50:42 -0500

    Cambio de servicios

--- src/components/Servicios/Servicios.jsx
+++ src/components/Servicios/Servicios.jsx
@@ -1,20 +1,38 @@
 import React from "react";
-import ServiceBar from "./ServiceBar";
-import * as Content from "./Content";
+import * as Content from "./Content2";
+import agricultura from "../../images/LOGOTIPOS_FINPEI_99.png";
+import crypto from "../../images/LOGOTIPOS_FINPEI_100.png";
+import stocks from "../../images/LOGOTIPOS_FINPEI_101.png";
+import startups from "../../images/LOGOTIPOS_FINPEI_102.png";
+import comerc from "../../images/LOGOTIPOS_FINPEI_103.png";
 
 const Servicios = () => {
   return (
-    <Content.Pararelogram id="servicios">
-      <Content.Wrapper>
+    <Content.Wrapper id="servicios">
         <Content.Title>SERVICIOS</Content.Title>
-        <Content.Text>
-          Somos un equipo experimentado de operadores dedicado a buscar y
-          detectar las mejores oportunidades de inversiones en los mercados
-          financieros de acciones americanas y mercado de divisas.
-        </Content.Text>
-        <ServiceBar />
-      </Content.Wrapper>
-    </Content.Pararelogram>
+        <Content.Container>
+        <Content.Item>
+        <Content.IconStyled src={agricultura}/>
+        <Content.Description agric>Agricultura</Content.Description>
+        </Content.Item>
+        <Content.Item>
+        <Content.IconStyled src={crypto}/>
+        <Content.Description>Crypto</Content.Description>
+        </Content.Item>
+        <Content.Item>
+        <Content.IconStyled src={stocks}/>
+        <Content.Description>Stocks</Content.Description>
+        </Content.Item>
+        <Content.Item>
+        <Content.IconStyled src={startups}/>
+        <Content.Description>Startups</Content.Description>
+        </Content.Item>
+        <Content.Item>
+        <Content.IconStyled src={comerc}/>
+        <Content.Description>Comercio exterior</Content.Description>
+        </Content.Item>
+        </Content.Container>
+    </Content.Wrapper>
   );
 };
 
