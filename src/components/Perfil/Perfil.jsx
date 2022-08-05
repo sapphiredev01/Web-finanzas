e65d7a2958483545d99c98538442c91e3d78a19d
Naruxtian <Naruxdev@Gmail.com>
2022-08-05 18:28:02 -0500

    cambio info perfiles

--- src/components/Perfil/Perfil.jsx
+++ src/components/Perfil/Perfil.jsx
@@ -17,14 +17,14 @@ const Paquetes = () => {
           <S.Card1>
             <S.CardTitle>Starter</S.CardTitle>
             <S.Description>
-              <li>Capital menor: $5,000 USD - $25,000 USD</li>
+              <li>Capital menor: $10,000 USD - $50,000 USD</li>
               <li>Horizonte de inversión de 6 a 12 meses</li>
             </S.Description>
           </S.Card1>
           <S.Card2>
             <S.CardTitle>Advanced</S.CardTitle>
             <S.Description>
-              <li>Mayor capital: $25,000 USD en adelante</li>
+              <li>Mayor capital: $50,000 USD en adelante</li>
               <li>Horizonte de inversión de 1 a 5 años</li>
             </S.Description>
           </S.Card2>
