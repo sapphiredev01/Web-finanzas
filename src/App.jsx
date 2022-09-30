e3a7a17b7d86c18a98a3fa7b654134cffa7074d3
Naruxtian <Naruxdev@Gmail.com>
2022-09-30 12:30:45 -0500

    Cambios imagenes

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,12 @@ import React from "react";
 import Hero from "./components/Hero/Hero";
 import Stocks from "./components/Stocks/Stocks";
 import Nosotros from "./components/Nosotros/Nosotros";
-import Servicios from "./components/Servicios/Servicios";
 import Vehiculos from "./components/Vehiculos/Vehiculos";
-import ContactoGod from "./components/Contacto/Contacto";
 import Contacto from "./components/Contacto2/Contacto";
 import Inversion from "./components/Inversion/Inversion";
-import Perfil from "./components/Perfil/Perfil";
 import Perfil2 from "./components/Perfil2/Perfil";
 import Nav from "./components/UI/Nav/Nav";
 import Media from "./components/UI/Media/Media";
-import Banner from "./components/Banner/Banner";
-import Inicio from "./components/Inicio/inicio";
 import "./App.css";
 import { useDesktop } from "./hooks/useDesktop";
 
